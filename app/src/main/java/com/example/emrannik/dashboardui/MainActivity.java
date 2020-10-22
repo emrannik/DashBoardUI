2710bb07e3a4393a6dc45aeb87da1494f033dea5
emran13 <emran.nannu@gmail.com>
2020-10-22 22:51:25 +0600

    Restaurant UI design template

---
+++ app/src/main/java/com/example/emrannik/dashboardui/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.emrannik.dashboardui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    TabLayout tabMenu;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabMenu     = findViewById(R.id.tabMenu);
+        viewPager   = findViewById(R.id.viewPager);
+
+        setupViewPager(viewPager);
+
+        tabMenu.setupWithViewPager(viewPager);
+        viewPager.setCurrentItem(1);  // 0 = drink , 1=food
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new DrinkFragment(),"Drink");
+        adapter.addFragment(new FoodFragment(),"Food");
+        viewPager.setAdapter(adapter);
+    }
+}
